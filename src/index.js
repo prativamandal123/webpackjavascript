3684349f6e434ecc4841de4f328e0804e7c0d88c
prativamandal123 <mandalprativa66@gmail.com>
2025-06-01 15:23:28 +0545

    push

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import sidebarItems from './sidebar-config.json';
 import './style.css';
 
-const sidebar = document.createElement('div');
-sidebar.className = 'sidebar';
+const sidebar = document.createElement('div'); //box
+sidebar.className = 'sidebar'; //class in css
 
 sidebarItems.forEach(item => {
   const el = document.createElement('div');
   el.className = 'sidebar-item';
-  el.innerHTML = `${item.icon} ${item.label}`;
+  el.innerHTML = `${item.icon} ${item.label}`; // call item
   el.dataset.type = item.type;
   sidebar.appendChild(el);
 });
